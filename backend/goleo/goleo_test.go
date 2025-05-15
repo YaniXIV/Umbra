e8896bc89ebfcb43c14982d82757977dcae61717
YaniXIV <YaniXIV.dev@gmail.com>
2025-05-14 20:09:13 -0600

    Testing

--- backend/goleo/goleo_test.go
+++ backend/goleo/goleo_test.go
@@ -1,12 +1,28 @@
 package goleo
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"testing"
+)
 
-func Testrun(t *testing.T) {
-	testrun()
+func TestInit(t *testing.T) {
+	lp, err := InitFromCircuitWithOptions(InitOptions{
+		ProjectName: "geoproof",
+		LeoBin:      "leo",
+		CircuitPath: "./GeoProof/src/main.leo",
+	})
+	if err != nil {
+		panic(err)
+	}
+	buildErr := lp.Build()
+	if buildErr != nil {
+		log.Println("build failed :", err)
+	}
 
-}
-
-func TestInit() {
-	InitFromCircuit()
+	out, runErr := lp.Run("3u32", "3u32", "12u32", "13u32", "50u32")
+	if runErr != nil {
+		log.Println("failed to run", out, runErr)
+	}
+	fmt.Println(out)
 }
